3e991272db0b926c4930f3d8207d2310211b945b
türkay ürkmez <turkay.urkmez@outlook.com>
2024-12-16 11:13:47 +0300

    init repo

---
+++ OOPOverview/ClassAndObjects/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    //Idea:
+    //Tanım:
+    public class Person
+    {
+        public string Name;
+        public byte Age;
+    }
+}
---
+++ OOPOverview/ClassAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassAndObjects;
+
+Console.WriteLine("Hello, World!");
+
+Person yazilimci = new Person();
+yazilimci.Name = "Türkay";
+yazilimci.Age = 44;
+
+Person muhasebeci = yazilimci;
+
+muhasebeci.Name = "Mehmet";
+muhasebeci.Age = 33;
+
+Console.WriteLine("Yazılımcı nesnesi:");
+Console.WriteLine(yazilimci.Name);
+Console.WriteLine(yazilimci.Age);
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("Muhasebeci nesnesi:");
+Console.WriteLine(muhasebeci.Name);
+Console.WriteLine(muhasebeci.Age);
+
+
